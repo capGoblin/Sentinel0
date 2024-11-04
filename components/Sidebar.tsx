86aee7157ae7b0eafa9839fac1c1d0ad967edae9
capGoblin <dharshan2457@gmail.com>
2024-11-04 20:16:43 +0530

    add handlesubmit inside component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,10 +7,9 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { Folder, Star, FileText, Plus } from "lucide-react";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import NewFolderDialog from "./NewFolderDialog";
 import { useStore } from "@/store/store";
-import { Blob, Indexer, getFlowContract } from "@0glabs/0g-ts-sdk";
 import { ethers } from "ethers-v5";
 import { hexlify } from "ethers-v5/lib/utils";
 import abi from "@/lib/abi.js";
@@ -25,6 +24,7 @@ export default function Sidebar() {
   const fileInputRef = useRef<HTMLInputElement>(null);
   const [showNewFolderDialog, setShowNewFolderDialog] = useState(false);
   const { addFile, addFolder, setCurrentPath, currentPath } = useStore();
+  const [sharedHash, setSharedHash] = useState<string>("");
   const { address, caipAddress, isConnected } = useAppKitAccount();
   const { walletProvider } = useAppKitProvider("eip155");
 
@@ -47,13 +47,13 @@ export default function Sidebar() {
       const rootHash = await uploadFile(file);
       console.log("File uploaded with root hash:", rootHash);
 
-      // const rootHash = "0x1234567890abcdef";
-      // const txHash = await handleSubmit(
-      //   `${currentPath}/${file.name}`,
-      //   rootHash,
-      //   "address"
-      // );
-      // console.log("Transaction hash:", txHash);
+
+      const txHash = await handleSubmit(
+        `${currentPath}/${file.name}`,
+        rootHash,
+        address!
+      );
+      console.log("Transaction hash:", txHash);
 
       // const query_tx = await handleQuery(
       //   `${currentPath}/${file.name}`,
@@ -70,6 +70,10 @@ export default function Sidebar() {
         size: formatFileSize(file.size),
         rootHash: rootHash, // Store the root hash for future reference
       });
+      console.log("File added to state:", file.name);
+      console.log("File root hash:", rootHash);
+      console.log("File type:", file.type);
+      console.log("File size:", file.size);
 
       // Reset input
       if (fileInputRef.current) {
@@ -83,6 +87,131 @@ export default function Sidebar() {
       setUploading(false);
     }
   };
+ 
+
+  async function handleSubmit(key: string, value: string, viewing_key: string) {
+    const routing_contract = "secret1muslwwvtf954257ltlpsnvtucu77vgqh8ndpux";
+    const routing_code_hash =
+      "3e94dd717d6ad2b01bdc65fbe27e90dfd62e6eb5f0474c8310f95bafe5ec3ae8";
+    const iface = new ethers.utils.Interface(abi);
+    // @ts-ignore
+    const provider = new ethers.providers.Web3Provider(walletProvider);
+
+    const [myAddress] = await provider.send("eth_requestAccounts", []);
+
+    const { userPrivateKeyBytes, userPublicKeyBytes, sharedKey } =
+      await generateKeys();
+
+    // @ts-ignore
+    const callbackSelector = iface.getSighash(
+      iface.getFunction("upgradeHandler")
+    );
+
+    console.log("callbackSelector: ", callbackSelector);
+
+    const callbackGasLimit = 90000;
+    // The function name of the function that is called on the private contract
+    const handle = "store_value";
+
+    // Data are the calldata/parameters that are passed into the contract
+    const data = JSON.stringify({
+      key: key,
+      value: value,
+      viewing_key: viewing_key,
+    });
+
+    const chainId = (await provider.getNetwork()).chainId.toString();
+
+    const publicClientAddress = await getPublicClientAddress(chainId);
+
+    const callbackAddress = publicClientAddress.toLowerCase();
+    console.log("callback address: ", callbackAddress);
+
+    // Payload construction
+    const payload = constructPayload(
+      data,
+      routing_contract,
+      routing_code_hash,
+      myAddress,
+      userPublicKeyBytes,
+      callbackAddress,
+      callbackSelector,
+      callbackGasLimit
+    );
+
+    const { ciphertext, payloadHash, payloadSignature, _info } =
+      await encryptPayload(
+        payload,
+        sharedKey,
+        provider,
+        myAddress,
+        userPublicKeyBytes,
+        routing_code_hash,
+        handle,
+        callbackGasLimit,
+        iface,
+        callbackSelector
+      );
+
+    const functionData = iface.encodeFunctionData("send", [
+      payloadHash,
+      myAddress,
+      routing_contract,
+      _info,
+    ]);
+
+    const feeData = await provider.getFeeData();
+    const maxFeePerGas = feeData.maxFeePerGas;
+    const maxPriorityFeePerGas = feeData.maxPriorityFeePerGas;
+    const gasFee =
+      maxFeePerGas && maxPriorityFeePerGas
+        ? maxFeePerGas.add(maxPriorityFeePerGas)
+        : await provider.getGasPrice();
+    let amountOfGas;
+    let my_gas = 150000;
+
+    if (chainId === "4202") {
+      amountOfGas = gasFee.mul(callbackGasLimit).mul(100000).div(2);
+    } else if (chainId === "128123") {
+      amountOfGas = gasFee.mul(callbackGasLimit).mul(1000).div(2);
+      my_gas = 15000000;
+    } else if (chainId === "1287") {
+      amountOfGas = gasFee.mul(callbackGasLimit).mul(1000).div(2);
+      my_gas = 15000000;
+    } else if (chainId === "300") {
+      amountOfGas = gasFee.mul(callbackGasLimit).mul(100000).div(2);
+      my_gas = 15000000;
+    } else if (chainId === "5003") {
+      amountOfGas = gasFee.mul(callbackGasLimit).mul(1000000).div(2);
+      my_gas = 1500000000;
+    } else if (chainId === "80002") {
+      amountOfGas = gasFee.mul(callbackGasLimit).mul(100).div(2);
+      console.log("amountOfGas: ", amountOfGas);
+    } else if (chainId === "1995") {
+      amountOfGas = gasFee.mul(callbackGasLimit).mul(100).div(2);
+      my_gas = 200000;
+    } else if (chainId === "713715") {
+      amountOfGas = gasFee.mul(callbackGasLimit).mul(100).div(2);
+      my_gas = 200000;
+    } else {
+      amountOfGas = gasFee.mul(callbackGasLimit).mul(3).div(2);
+    }
+
+    const tx_params = {
+      gas: hexlify(my_gas),
+      to: publicClientAddress,
+      from: myAddress,
+      value: hexlify(amountOfGas),
+      data: functionData,
+    };
+
+    console.log("tx_params: ", tx_params.value);
+
+    const txHash = await provider.send("eth_sendTransaction", [tx_params]);
+    console.log(`Transaction Hash: ${txHash}`);
+
+    return txHash;
+  }
 
   const formatFileSize = (bytes: number): string => {
     if (bytes === 0) return "0 B";
@@ -108,7 +237,6 @@ export default function Sidebar() {
     const data = await response.json();
     return data.rootHash; // Return the root hash from the response
   }
-
 
   return (
     <div className="w-64 border-r p-4">
@@ -188,8 +316,6 @@ export default function Sidebar() {
   );
 }
 
-
-
 const handleQuery = async (key: string, viewingKey: string) => {
   let query_tx;
   try {
@@ -199,9 +325,9 @@ const handleQuery = async (key: string, viewingKey: string) => {
     });
 
     query_tx = await secretjs.query.compute.queryContract({
-      contract_address: "secret1j0gpu6tlwnc9fw55wcfsfuml00kqpcnqz7dck7",
+      contract_address: "secret1muslwwvtf954257ltlpsnvtucu77vgqh8ndpux",
       code_hash:
-        "6311a3f85261fc720d9a61e4ee46fae1c8a23440122b2ed1bbcebf49e3e46ad2",
+        "3e94dd717d6ad2b01bdc65fbe27e90dfd62e6eb5f0474c8310f95bafe5ec3ae8",
       query: {
         retrieve_value: {
           key: key,
@@ -218,128 +344,29 @@ const handleQuery = async (key: string, viewingKey: string) => {
   return query_tx;
 };
 
+const handleKeysQuery = async (viewingKey: string) => {
+  let query_tx;
+  try {
+    const secretjs = new SecretNetworkClient({
+      url: "https://lcd.testnet.secretsaturn.net",
+      chainId: "pulsar-3",
+    });
 
-export async function handleSubmit(key: string, value: string, viewing_key: string) {
-  const routing_contract = "secret1j0gpu6tlwnc9fw55wcfsfuml00kqpcnqz7dck7";
-  const routing_code_hash =
-    "6311a3f85261fc720d9a61e4ee46fae1c8a23440122b2ed1bbcebf49e3e46ad2";
-  const iface = new ethers.utils.Interface(abi);
-  // @ts-ignore
-  const provider = new ethers.providers.Web3Provider(walletProvider);
+    query_tx = await secretjs.query.compute.queryContract({
+      contract_address: "secret1muslwwvtf954257ltlpsnvtucu77vgqh8ndpux",
+      code_hash:
+        "3e94dd717d6ad2b01bdc65fbe27e90dfd62e6eb5f0474c8310f95bafe5ec3ae8",
+      query: {
+        retrieve_keys: {
+          viewing_key: viewingKey,
+        },
+      },
+    });
 
-
-  const [myAddress] = await provider.send("eth_requestAccounts", []);
-
-  const { userPrivateKeyBytes, userPublicKeyBytes, sharedKey } =
-    await generateKeys();
-
-  // @ts-ignore
-  const callbackSelector = iface.getSighash(
-    iface.getFunction("upgradeHandler")
-  );
-
-  console.log("callbackSelector: ", callbackSelector);
-
-  const callbackGasLimit = 90000;
-  // The function name of the function that is called on the private contract
-  const handle = "store_value";
-
-  // Data are the calldata/parameters that are passed into the contract
-  const data = JSON.stringify({
-    key: key,
-    value: value,
-    viewing_key: viewing_key,
-  });
-
-  const chainId = (await provider.getNetwork()).chainId.toString();
-
-  const publicClientAddress = await getPublicClientAddress(chainId);
-
-  const callbackAddress = publicClientAddress.toLowerCase();
-  console.log("callback address: ", callbackAddress);
-
-  // Payload construction
-  const payload = constructPayload(
-    data,
-    routing_contract,
-    routing_code_hash,
-    myAddress,
-    userPublicKeyBytes,
-    callbackAddress,
-    callbackSelector,
-    callbackGasLimit
-  );
-
-  const { ciphertext, payloadHash, payloadSignature, _info } =
-    await encryptPayload(
-      payload,
-      sharedKey,
-      provider,
-      myAddress,
-      userPublicKeyBytes,
-      routing_code_hash,
-      handle,
-      callbackGasLimit,
-      iface,
-      callbackSelector
-    );
-
-  const functionData = iface.encodeFunctionData("send", [
-    payloadHash,
-    myAddress,
-    routing_contract,
-    _info,
-  ]);
-
-  const feeData = await provider.getFeeData();
-  const maxFeePerGas = feeData.maxFeePerGas;
-  const maxPriorityFeePerGas = feeData.maxPriorityFeePerGas;
-  const gasFee =
-    maxFeePerGas && maxPriorityFeePerGas
-      ? maxFeePerGas.add(maxPriorityFeePerGas)
-      : await provider.getGasPrice();
-  let amountOfGas;
-  let my_gas = 150000;
-
-  if (chainId === "4202") {
-    amountOfGas = gasFee.mul(callbackGasLimit).mul(100000).div(2);
-  } else if (chainId === "128123") {
-    amountOfGas = gasFee.mul(callbackGasLimit).mul(1000).div(2);
-    my_gas = 15000000;
-  } else if (chainId === "1287") {
-    amountOfGas = gasFee.mul(callbackGasLimit).mul(1000).div(2);
-    my_gas = 15000000;
-  } else if (chainId === "300") {
-    amountOfGas = gasFee.mul(callbackGasLimit).mul(100000).div(2);
-    my_gas = 15000000;
-  } else if (chainId === "5003") {
-    amountOfGas = gasFee.mul(callbackGasLimit).mul(1000000).div(2);
-    my_gas = 1500000000;
-  } else if (chainId === "80002") {
-    amountOfGas = gasFee.mul(callbackGasLimit).mul(100).div(2);
-    console.log("amountOfGas: ", amountOfGas);
-  } else if (chainId === "1995") {
-    amountOfGas = gasFee.mul(callbackGasLimit).mul(100).div(2);
-    my_gas = 200000;
-  } else if (chainId === "713715") {
-    amountOfGas = gasFee.mul(callbackGasLimit).mul(100).div(2);
-    my_gas = 200000;
-  } else {
-    amountOfGas = gasFee.mul(callbackGasLimit).mul(3).div(2);
+    console.log(query_tx);
+  } catch (error) {
+    console.error("Query failed", error);
   }
 
-  const tx_params = {
-    gas: hexlify(my_gas),
-    to: publicClientAddress,
-    from: myAddress,
-    value: hexlify(amountOfGas),
-    data: functionData,
-  };
-
-  console.log("tx_params: ", tx_params.value);
-
-  const txHash = await provider.send("eth_sendTransaction", [tx_params]);
-  console.log(`Transaction Hash: ${txHash}`);
-
-  return txHash;
-}
+  return query_tx;
+};
